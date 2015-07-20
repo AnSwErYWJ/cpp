c2828490dc5f43801eb24aad4da762f3b210ce49
AnSwErYWJ <ywj1993@yeah.net>
2015-07-20 18:54:23 +0800

    20150720

---
+++ size.cpp
@@ -0,0 +1,30 @@
+/*************************************************************************
+	> File Name: size.cpp
+	> Author: AnSwEr
+	> Mail: 1045837697@qq.com
+	> Created Time: 2015年07月20日 星期一 18时42分53秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main(void)
+{
+    std::cout<<"The size of short is :\t\t";
+    std::cout<<sizeof(short)<<"bytes\n";
+    std::cout<<"The size of int is :\t\t";
+    std::cout<<sizeof(int)<<"bytes\n";
+    std::cout<<"The size of long is :\t\t";
+    std::cout<<sizeof(long)<<"bytes\n";
+    std::cout<<"The size of long long int is :\t\t";
+    std::cout<<sizeof(long long int)<<"bytes\n";
+    std::cout<<"The size of char is :\t\t";
+    std::cout<<sizeof(char)<<"bytes\n";
+    std::cout<<"The size of bool is :\t\t";
+    std::cout<<sizeof(bool)<<"bytes\n";
+    std::cout<<"The size of float is :\t\t";
+    std::cout<<sizeof(float)<<"bytes\n";
+    std::cout<<"The size of double is :\t\t";
+    std::cout<<sizeof(double)<<"bytes\n";
+
+    return 0;
+}
