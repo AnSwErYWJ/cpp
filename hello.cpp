4d94d302ba8dc5028b9f20a13c6d6af7c58ad47f
AnSwErYWJ <ywj1993@yeah.net>
2015-07-20 16:51:38 +0800

    20150720

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: hello.cpp
+	> Author: AnSwEr
+	> Mail: 1045837697@qq.com
+	> Created Time: 2015年07月20日 星期一 16时42分44秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+    std::cout<<"Hello World!\n";
+    return 0;
+}
